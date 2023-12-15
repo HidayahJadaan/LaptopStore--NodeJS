d202f5326da6bc1d359a9bea2cc2cbe2e026621f
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-15 19:39:53 +0300

    Laptop Page is Finished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+// SYNC. VERSION HERE DOSEN'T MAKE A PROBLEM; IT IS ONLY HAPPEN ONCE
 const jsonData = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(jsonData);
 
@@ -19,19 +20,44 @@ const server = http.createServer((req, res) => {
   //   const query = url.parse(req.url, true).query;
   const id = url.parse(req.url, true).query.id;
 
-  if (pathName === "/product" || pathName === "/") {
+  //   PRODUCT OVERVIEW
+  if (pathName === "/products" || pathName === "/") {
     // Send response back to the server
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    res.end("Welcome in product page");
-  } else if (pathName === "/laptop" && id< laptopData.length) {
+
+    fs.readFile(`${__dirname}/templates/overview.html`,"utf-8",(err, data) => {
+        let overviewOutput =data;
+
+        fs.readFile(`${__dirname}/templates/card.html`,"utf-8",(err, data) => {
+            const cardsOutput = laptopData.map((currentLaptop) =>replaceTemplate(data, currentLaptop).join(''));
+            overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+
+            res.end(overviewOutput);
+          }
+        );
+      }
+    );
+
+    // res.end(`Welcome in product page`);
+  }
+  //   LAPTOP DETAILS
+  else if (pathName === "/laptop" && id < laptopData.length) {
     // Send response back to the server
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    res.end(`Welcome in laptop page fpr laptop ${id}`);
-  } else {
+
+    // NOT GO TO THE Sync. VERSION --> IT IS ALWAYS BLOCKING (MAKES THE ENTIRE CODE STOPS FOR THE TIME THAT THE FUNCTION DOINF IT'S WORK and thats block the entire thread for all the users that they are accessing your code --> 1 THREAD at a time)
+    fs.readFile(`${__dirname}/templates/laptop.html`, "utf-8", (err, data) => {
+      const laptop = laptopData[id];
+      const output = replaceTemplate(data, laptop);
+      res.end(output);
+    });
+  }
+  //   URL NOT FOUND
+  else {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
@@ -46,3 +72,19 @@ server.listen(3000, () => {
 
 // 3. Create a route for the server(Responed in diferent ways for different URLs)
 // ==> DO IT IN THE SERVER CALLBACK FUNCTION
+
+// ==============================
+
+function replaceTemplate(orignalHTML, laptop) {
+  let output = orignalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%DECRIPTION%}/g, laptop.description);
+  output = output.replace(/{%ID%}/g, laptop.id);
+
+  return output;
+}
